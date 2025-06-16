8b4a8f929af8007255f6519db3f3c448da3fce60
amaannawab923 <amaannawab923@gmail.com>
2025-06-16 12:20:53 +0530

    Icon components

--- superset-frontend/plugins/plugin-chart-ag-grid-table/src/AgGridTable/components/CustomHeader.tsx
+++ superset-frontend/plugins/plugin-chart-ag-grid-table/src/AgGridTable/components/CustomHeader.tsx
@@ -29,27 +29,8 @@ import {
 import { IHeaderParams, Column, ColDef } from 'ag-grid-community';
 import CustomPopover from './CustomPopover';
 import { CustomColDef } from '..';
-
-const ThreeDots = ({ size = 14, color = 'black' }) => (
-  <svg
-    width={size}
-    height={size}
-    viewBox="0 0 16 16"
-    fill={color}
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <circle cx="8" cy="3" r="1.2" />
-    <circle cx="8" cy="8" r="1.2" />
-    <circle cx="8" cy="13" r="1.2" />
-  </svg>
-);
-const FilterIcon = (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-    <rect x="3" y="6" width="18" height="2" rx="1" />
-    <rect x="6" y="11" width="12" height="2" rx="1" />
-    <rect x="9" y="16" width="6" height="2" rx="1" />
-  </svg>
-);
+import FilterIcon from './Filter';
+import KebabMenu from './KebabMenu';
 
 // Styled Components
 const Container = styled.div`
@@ -336,7 +317,7 @@ const CustomHeader: React.FC<CustomHeaderParams> = ({
           onClose={() => setIsMenuVisible(false)}
         >
           <div className="three-dots-menu" onClick={handleMenuClick}>
-            <ThreeDots />
+            <KebabMenu />
           </div>
         </CustomPopover>
       )}
